dedd56a9614b752ca4c304a4301a14caaf5c8cac
NsLonger <15810781521@163.com>
2020-03-05 11:50:20 +0800

    初建工程

---
+++ MeiPian/AppDelegate/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  MeiPian
+//
+//  Created by EGLS_BMAC on 2020/3/5.
+//  Copyright © 2020 EGLS_BMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '8.0'
+use_frameworks!
+
+def all_Pods
+    pod 'YYKit'
+    pod 'AFNetworking'
+    pod 'MBProgressHUD'
+    pod 'MJRefresh'
+    pod 'MJExtension'
+    pod 'AvoidCrash', '~> 2.5.2'
+    pod "PYSearch"
+    pod 'MLeaksFinder'
+end
+
+target 'MeiPian' do
+   all_Pods
+end
