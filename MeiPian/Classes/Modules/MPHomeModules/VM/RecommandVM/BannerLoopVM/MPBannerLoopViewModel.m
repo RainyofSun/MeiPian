2a50be7faaa216665999d91b415d070cdf8c0efa
刘冉 <liuran@liurandeMBP.lan>
2020-04-05 13:21:44 +0800

    Sync

---
+++ MeiPian/Classes/Modules/MPHomeModules/VM/RecommandVM/BannerLoopVM/MPBannerLoopViewModel.m
@@ -0,0 +1,63 @@
+//
+//  MPBannerLoopViewModel.m
+//  MeiPian
+//
+//  Created by 刘冉 on 2020/4/5.
+//  Copyright © 2020 EGLS_BMAC. All rights reserved.
+//
+
+#import "MPBannerLoopViewModel.h"
+
+@interface MPBannerLoopViewModel ()
+
+/** loopModelSource */
+@property (nonatomic,strong) NSArray <MPBannerLoopModel *>*loopModelSource;
+/** loopImgSource */
+@property (nonatomic,strong) NSMutableDictionary *loopImgSource;
+
+@end
+
+@implementation MPBannerLoopViewModel
+
+- (instancetype)init{
+    if (self = [super init]) {
+        [self getDefaultData];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    NSLog(@"DELLOC : %@",NSStringFromClass(self.class));
+}
+
+#pragma mark - public methods
+// 轮播图数据
+- (NSDictionary *)bannerLoopSource {
+    return [self.loopImgSource copy];
+}
+
+// 点击轮播图事件处理
+- (NSString *)touchBannerLoopLink:(NSInteger)index {
+    return self.loopModelSource[index].uri;
+}
+
+#pragma mark - private methods
+- (void)getDefaultData {
+    WeakSelf;
+    [MPLocalData MPGetLocalDataFileName:@"BannerLoop0" localData:^(id  _Nonnull responseObject) {
+        weakSelf.loopModelSource = [MPBannerLoopModel modelArrayWithDictArray:(NSArray *)responseObject];
+        [weakSelf combineLoopSource];
+    } errorBlock:^(NSString * _Nullable errorInfo) {
+        
+    }];
+}
+
+- (void)combineLoopSource {
+    self.loopImgSource = [NSMutableDictionary dictionaryWithCapacity:self.loopModelSource.count];
+    WeakSelf;
+    [self.loopModelSource enumerateObjectsUsingBlock:^(MPBannerLoopModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [weakSelf.loopImgSource setObject:obj.cover_img forKey:obj.title];
+    }];
+}
+
+@end
--- MeiPian/Classes/Modules/MPHomeModules/View/RecommandView/SheYingView/MPSheYingBannerLoopView.m
+++ MeiPian/Classes/Modules/MPHomeModules/View/RecommandView/SheYingView/MPSheYingBannerLoopView.m
@@ -7,18 +7,74 @@
 //
 
 #import "MPSheYingBannerLoopView.h"
+#import "MPBannerLoopViewModel.h"
+
+@interface MPSheYingBannerLoopView ()<AppCycleScrollviewDelegate>
+{
+    CGFloat LoopW;
+}
+/** imgBannerView */
+@property (nonatomic,strong) AppCycleScrollview *imgBannerView;
+/** loopVM */
+@property (nonatomic,strong) MPBannerLoopViewModel *loopVM;
+
+@end
 
 @implementation MPSheYingBannerLoopView
 
 - (instancetype)init {
     if (self = [super init]) {
-        self.backgroundColor = [UIColor redColor];
+        LoopW = ScreenWidth - 30;
+        [self setupImgBannerView];
+        [self getLoopImgSource];
     }
     return self;
 }
 
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    [self.imgBannerView autoAlignAxisToSuperviewAxis:ALAxisVertical];
+    [self.imgBannerView autoPinEdge:ALEdgeTop toEdge:ALEdgeTop ofView:self withOffset:10];
+    [self.imgBannerView autoSetDimensionsToSize:CGSizeMake(LoopW, LoopW * 0.36)];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.imgBannerView.placeholderImage = [UIImage imageNamed:@"image_loading_wide"];
+    });
+}
+
 - (void)dealloc {
     NSLog(@"DELLOC : %@",NSStringFromClass(self.class));
+}
+
+#pragma mark - AppCycleScrollviewDelegate
+- (void)cycleScrollView:(AppCycleScrollview *)cycleScrollView didSelectItemAtIndex:(NSInteger)index {
+    [self.loopVM touchBannerLoopLink:index];
+}
+
+#pragma mark - private methods
+- (void)setupImgBannerView {
+    
+    self.imgBannerView = [AppCycleScrollview appCycleScrollViewWithFrame:CGRectZero shouldInfiniteLoop:YES cycleDelegate:self];
+    self.imgBannerView.imgCornerRadius = 5.0f;
+    self.imgBannerView.autoScrollTimeInterval = 5.0f;
+    self.imgBannerView.isZoom = NO;
+//    self.imgBannerView.itemSpace = 10.f;
+    self.imgBannerView.heightProportion = 0.875;
+    self.imgBannerView.itemWidth = LoopW;
+    self.imgBannerView.isHidePageControl = NO;
+    [self addSubview:self.imgBannerView];
+}
+
+- (void)getLoopImgSource {
+    self.imgBannerView.imageURLStringsGroup = [self.loopVM bannerLoopSource].allValues;
+}
+
+#pragma mark - lazy
+- (MPBannerLoopViewModel *)loopVM {
+    if (!_loopVM) {
+        _loopVM = [[MPBannerLoopViewModel alloc] init];
+    }
+    return _loopVM;
 }
 
 @end
